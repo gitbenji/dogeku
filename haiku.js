19913d336c400c393f2c63fd8e62c8ec86508155
shvagahb <biancaaidass@gmail.com>
2016-01-24 00:48:50 -0500

    sadas

---
+++ haiku.js
@@ -0,0 +1,105 @@
+
+function makeHaiku(term, syll);
+
+Math.floor((Math.random())
+var z = Math.random();
+
+//first line
+
+if (syll[1] == 1 && z = 0){
+	console.log(1syll + term[i] + 3syll);
+}
+else if (syll[1] == 1 && z == 1){
+	console.log(2syll + 2syll + term[i]);
+}
+
+else if (syll[1] == 2 && z == 0){
+	console.log(1syll + 1syll + term[i]);
+}
+
+else if (syll[1] == 2 && z == 1){
+	console.log(1syll + term[i] + 1syll);
+
+}
+else if (syll[1] == 3 && z == 0){
+	console.log(1syll + 1syll + term[i]);
+}
+else if (syll[1] == 3&& z == 1){
+	console.log(2syll + term[i]);
+}
+
+//second line first word
+
+if (syll[2] == 1 && z == 0){
+	console.log(2syll + term[i]);
+}
+
+else if (syll[2] == 1 && z == 1){
+	console.log(3syll + term[i]);
+}
+
+else if (syll[2] == 2 && z == 0){
+	console.log(1syll + 1syll + term[i]);
+}
+
+else if (syll[2] == 2 && z == 1){
+	console.log(1syll + 1syll + term[i]);
+}
+
+else if (syll[2] == 3 && z == 0){
+	console.log(2syll + term[i]);
+} 
+else if (syll[2] == 3 && z == 1){
+	console.log(2syll + term[i]);
+}
+
+//second line second word
+
+if (syll[3] == 1 && z == 0){
+	console.log(2syll + term[i] + 1syll);
+}
+else if (syll[3] == 1 && z == 1){
+	console.log(2syll + term[i]);
+}
+
+else if (syll[3] == 2 && z == 0){
+	console.log(1syll + term[i]);
+}
+
+else if (syll[3] == 2 && z == 1){
+	console.log(term[i] + 1syll);
+}
+
+else if (syll[3] == 3 && z == 0){
+	console.log(2syll + term[i] + 1syll);
+}
+
+else if (syll[3] == 3 && z == 1){
+	console.log(1syll + 1syll + term[i] + 1syll);
+}
+
+//third line
+
+if (syll[4] == 1 && z == 0){
+	console.log(2syll + term[i] + 2syll);
+}
+
+else if (syll[4] == 1 && z == 1){
+	console.log(3syll + 1syll +term[i]);
+}
+
+else if (syll[4] == 2 && z == 0){
+	console.log(2syll + term[i] + 1syll);
+}
+
+else if (syll[4] == 2 && z == 1){
+	console.log(1syll + term[i] + 2syll);
+}
+
+else if (syll[4] == 3 && z == 0){
+	console.log(1syll + term[i] + 1syll);
+}
+
+else if (syll[4] == 3 && z == 1){
+	console.log(term[i] + 1syll + 1syll);
+}
